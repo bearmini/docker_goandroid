abeed2d5685f21f5760be65c71ae7978d6549ce5
Takashi Oguma <bear.mini@gmail.com>
2014-10-06 11:10:05 +0900

    add docker-enter function definition to ensure enter.sh works on boot2docker without dependency on a platform

--- enter.sh
+++ enter.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+docker_enter() {
+    boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
+    boot2docker ssh -t sudo /var/lib/boot2docker/docker-enter "$@"
+}
+
 CID=$(docker ps --latest --quiet)
-sudo docker-enter $CID /bin/bash
+docker_enter $CID /bin/bash
